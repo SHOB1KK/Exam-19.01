c70212f021cfd4eea75cc7fa1213e6e5ae435e1a
SHOB1KK <shobadalovyusuf018@gmail.com>
2025-01-19 14:11:53 +0500

    Program.cs

--- TASK7/Program.cs
+++ TASK7/Program.cs
@@ -1,5 +1,35 @@
-﻿Book book_1 = new Book();
-book_1.Author();
-book_1.Title();
-book_1.Year();
+Book book = new Book("NAgzay", "Huvaido", 2025);
+int year = Convert.ToInt32(Console.ReadLine());
+book.Inf();
+bool bln = book.Isnew(year);
+if(bln){
+    System.Console.WriteLine("да");
+}
+else{
+    System.Console.WriteLine("нет");
+}
+class Book{
+    public string Title;
+    public string Authot;
+    public int Year;
+
+    public Book(string title, string authot, int year){
+        Title = title;
+        Authot = authot;
+        Year = year;
+    }
+
+    public void Inf(){
+        System.Console.WriteLine($"Book = {Title}, Authour = {Authot}, Year = {Year}");
+    }
+
+    public bool Isnew(int year){
+        if(year > 1800){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
 
